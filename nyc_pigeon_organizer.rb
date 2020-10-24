c6ac184dc3d3d8c16fc501428981e7c334bd720e
Ori Markowitz <ori.markowitz@gmail.com>
2020-10-24 16:05:55 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized_hash = data.each_with_object({}) do |(key, value), hash|
+    value.each do |inner_key, names|
+      names.each do |name|
+        if !hash[name]
+          hash[name] = {}
+        end
+        if !hash[name][key]
+          hash[name][key] = []
+        end
+        hash[name][key].push(inner_key.to_s)
+      end
+    end
+  end
 end
