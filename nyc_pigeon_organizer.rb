276a7ce803d93e2c01e4d40c1e67dbee5faf24ee
Ori Markowitz <ori.markowitz@gmail.com>
2020-10-24 16:01:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  binding.pry
+  organized_hash = data.each_with_object({}) do |(key, value), array|
+    binding.pry
+    value.each do |inner_key, names|
+      binding.pry
+      names.each do |name|
+        binding.pry
+        if !array[name]
+          array[name] = {}
+          binding.pry
+        end
+        if !array[name][key]
+          array[name][key] = []
+          binding.pry
+        end
+        array[name][key].push(inner_key.to_s)
+        binding.pry
+      end
+    end
+  end
 end
